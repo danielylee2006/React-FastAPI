076902bae7c4b166f1e72ed0aee79dfa394e9ee4
Daniel Lee <danielyedarmlee@gmail.com>
2025-07-18 10:31:04 -0400

    fastAPI setup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware #Cross-Orgin Resource Sharing
 
+app = FastAPI(
+    title = "Choose your Own Adventure Game API",
+    description = "api to generate stories",
+    version = "0.1.0", 
+    #path to API documentation (fastAPI automatically creates documentation page)
+    docs_url = "/docs", #interactive documentation page (you can click on url paths)
+    redoc_url = "/redoc", #read-only documentation page (better for reference)
+)
 
-if __name__ == "__main__":
-    main()
+#middleware controls which external orgins (websites) are allowed to access this API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #allowing every website/domain to access this API (avoiding project complication)
+    allow_credentials=True,
+    allow_methods=["*"], #allow all HTTP methods to be used in requests to this API (GET, POST, PUT, etc.)
+    allow_headers=["*"] #allows requests to include any headers (headers carry metadata; ex: Content-Type)
+)
+
+if __name__ == "__main__": 
+    import uvicorn #web server
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
